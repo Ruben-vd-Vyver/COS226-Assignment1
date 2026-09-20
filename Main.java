0868ec2b706402f0da0bdd6deded9b9ea34e73bc
Matthew Malan <malan.matth@gmail.com>
2026-09-20 19:56:14 +0200

    Update number of threads and change lock implementation

--- Main.java
+++ Main.java
@@ -3,11 +3,11 @@ public class Main
 
     public static void main(String[] args) throws InterruptedException 
     {
-        int numberOfThreads = 0; /*Change*/
+        int numberOfThreads = 2; /*Change*/
         int iterations = 200;
 
         Auction auction =new Auction(AuctionUtils.generateItemName());
-        Lock lock = new Lock(); /*Add your lock here*/
+        Lock lock = new MCSLock(); /*Add your lock here*/
         Runner runner = new Runner(numberOfThreads,iterations,auction,lock);
         runner.run();
     }
